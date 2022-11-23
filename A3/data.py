067a143fcb5103144bca019e7878f5a7fe4213c0
Théau Blanchard <blanchard.theau@live.fr>
2022-11-23 15:38:38 +0100

    proper cropping + reduce lr on plateau val loss

--- A3/data.py
+++ A3/data.py
@@ -10,15 +10,38 @@ from torchvision.models import ResNet50_Weights
 
 #data_transforms = ResNet50_Weights.IMAGENET1K_V2.transforms
 
+def center_crop_main_axis(image):
+    """Center crop the image on the smallest axis. Basicaly extract the center square."""
+    if image.shape[-1] > image.shape[-2]:
+        res = transforms.CenterCrop((image.shape[-2], image.shape[-2]))(image)
+    else:
+        res = transforms.CenterCrop((image.shape[-1], image.shape[-1]))(image)
+    return res
+
+class CenterCropMainAxis:
+
+    def __init__(self) -> None:
+        pass
+
+    def __call__(self, image):
+        return center_crop_main_axis(image)
+        
+
 data_transforms = transforms.Compose([
+    # data augmentation
     transforms.RandomRotation(30),
-    transforms.GaussianBlur(5, sigma=(0.1, 2.0)),
+    transforms.GaussianBlur(3, sigma=(0.01, 2.0)),
+    transforms.RandomHorizontalFlip(0.5),
+    transforms.ToTensor(),
+    # properly crop the data to avoid stretching
+    CenterCropMainAxis(),
+    # Res net transforms
     transforms.Resize((232, 232)),
     transforms.CenterCrop((224, 224)),
-    transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225]),
-    transforms.randomHorizontalFlip(0.5),
 ])
+
+
 
 
